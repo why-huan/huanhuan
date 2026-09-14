f0209e1c7089c18f519893f9d171fd2c03e0a106
wuhuanyu.1 <wuhuanyu.1@jd.com>
2026-09-14 19:01:21 +0800

    Site updated: 2026-09-14 19:01:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/05/GeoServer Jxpath RCE/","2024/07/09/CC链学习/","2024/05/28/从内存马到内核马到Rookit/","2024/06/20/Linux下的隐藏进程/"];function toRandomPost(){
+var posts=["2024/07/09/CC链学习/","2024/06/20/Linux下的隐藏进程/","2024/07/05/GeoServer Jxpath RCE/","2024/05/28/从内存马到内核马到Rookit/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
